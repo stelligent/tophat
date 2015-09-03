3dc8e016322d9f29c333c47b9ecd5e40c3c20f4b
jonny sywulak <jonny@stelligent.com>
2015-09-02 22:03:20 -0700

    removes job seed job creation.
    this didn't work out for codepipeline, which was the original goal of tophat. It might be worthwhile to put back in at some other time.

--- cookbooks/jenkins-config/recipes/default.rb
+++ cookbooks/jenkins-config/recipes/default.rb
@@ -3,7 +3,7 @@ include_recipe 'java'
 include_recipe 'git'
 include_recipe 'jenkins::master'
 include_recipe 'jenkins-config::jenkins-plugins'
-include_recipe 'jenkins-config::jenkins-jobs'
+# include_recipe 'jenkins-config::jenkins-jobs'
 
 service 'jenkins' do
   action :restart
