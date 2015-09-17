dde5de116ca5da7e1bebf3bf67def19f036da983
Vincent Rivellino <vincent.rivellino@stelligent.com>
2015-09-17 12:20:26 -0400

    Install chefdk

--- cookbooks/jenkins-config/recipes/jenkins-yum-prereqs.rb
+++ cookbooks/jenkins-config/recipes/jenkins-yum-prereqs.rb
@@ -2,3 +2,12 @@ packages = %w{patch libyaml-devel glibc-headers autoconf gcc-c++ glibc-devel rea
 packages.each do |package|
   package package
 end
+
+remote_file "#{Chef::Config[:file_cache_path]}/chefdk.rpm" do
+    source "https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chefdk-0.7.0-1.el6.x86_64.rpm"
+    action :create
+end
+
+package "chefdk" do
+    source "#{Chef::Config[:file_cache_path]}/chefdk.rpm"
+end
