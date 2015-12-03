424bd33f9e9752a0562fd6a373dec9ba73c34d84
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2015-12-03 13:07:13 -0500

    Reference attribute for cookbook

--- cookbooks/jenkins-config/recipes/prereqs.rb
+++ cookbooks/jenkins-config/recipes/prereqs.rb
@@ -1,4 +1,4 @@
-packages = %w{patch libyaml-devel glibc-headers autoconf gcc-c++ glibc-devel readline-devel zlib-devel libffi-devel openssl-devel automake libtool bison sqlite-devel python-pip git mlocate}
+packages = node.default['jenkins-config']['prereqs']['packages']
 packages.each do |package|
   package package
 end
