23967774bc32b24372bcef0af6c5deefef005d62
Vincent Rivellino <vince79@gmail.com>
2015-12-09 20:56:48 -0500

    Add test kitchen and tests to tophat

---
+++ test/integration/default/serverspec/jenkins_service_spec.rb
@@ -0,0 +1,20 @@
+require 'serverspec'
+
+set :backend, :exec
+
+describe service('jenkins') do
+  it { should be_enabled }
+end
+
+describe service('jenkins') do
+  it { should be_running }
+end
+
+describe port(8080) do
+  it { should be_listening.with('tcp') }
+end
+
+describe command('curl -s http://127.0.0.1:8080/') do
+	its(:exit_status) { should eq 0 }
+  its(:stdout) { should match /(Jenkins|Authentication required)/ }
+end
