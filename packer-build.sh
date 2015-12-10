b82a560c4f742d63cf57c6f6bf0051a6b5d5c864
Vincent Rivellino <vince79@gmail.com>
2015-12-09 22:12:04 -0500

    Add helper script; use cache dir for berkshelf

---
+++ packer-build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -ex
+
+cd "$(dirname "$0")"
+berks_cookbook_path="./.berkscache"
+
+for arg in "$@"; do
+    if [[ "$arg" = sg-* ]]; then
+        TOPHAT_SG="$arg"
+    elif [[ "$arg" = subnet-* ]]; then
+        TOPHAT_SUBNET_ID="$arg"
+    fi
+done
+
+subnet_id="$TOPHAT_SUBNET_ID"
+sg_id="$TOPHAT_SG"
+vpc_id="$(aws ec2 describe-subnets --subnet-ids $subnet_id --query 'Subnets[0].VpcId' --output=text)"
+
+test -n "$vpc_id"
+test -n "$subnet_id"
+test -n "$sg_id"
+
+[ -d "$berks_cookbook_path" ] || mkdir "$berks_cookbook_path"
+
+berks vendor "$berks_cookbook_path"
+packer build \
+    -var "vpc_id=$vpc_id" \
+    -var "subnet_id=$subnet_id" \
+    -var "sg_id=$sg_id" \
+    -var "berks_cookbooks_path=$berks_cookbook_path" \
+    jenkins.json
