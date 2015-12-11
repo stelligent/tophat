0d855e421573dec5c8ae37436a9f283be1b47872
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2015-12-11 12:52:20 -0500

    Split cookbook into 2 primary recipes
    jenkins-config::master for Jenkins master nodes
    jenkins-config::slave for Jenkins slaves

--- cookbooks/jenkins-config/recipes/default.rb
+++ cookbooks/jenkins-config/recipes/default.rb
@@ -2,23 +2,14 @@ if ['rhel'].include?(node['platform_family'])
   # XXX should this instead do security updates instead of full
   # system updates?
   execute 'yum upgrade -y'
-  # default repository should be the Long-Term Support release
-  # https://wiki.jenkins-ci.org/display/JENKINS/LTS+Release+Line
-  node.default['jenkins']['master']['repository'] = 'http://pkg.jenkins-ci.org/redhat-stable'
-  node.default['jenkins']['master']['repository_key'] = 'http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key'
 end
 if ['debian'].include?(node['platform_family'])
-  node.default['apt']['compile_time_update'] = true
   include_recipe 'apt::default'
   # XXX should this instead do security updates instead of full
   # system updates?
   execute 'apt-get upgrade -y' do
     environment ({'DEBIAN_FRONTEND' => 'noninteractive'})
   end
-  # default repository should be the Long-Term Support release
-  # https://wiki.jenkins-ci.org/display/JENKINS/LTS+Release+Line
-  node.default['jenkins']['master']['repository'] = 'http://pkg.jenkins-ci.org/debian-stable'
-  node.default['jenkins']['master']['repository_key'] = 'http://pkg.jenkins-ci.org/debian-stable/jenkins-ci.org.key'
 end
 
 include_recipe 'jenkins-config::prereqs'
