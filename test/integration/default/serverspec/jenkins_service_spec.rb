4d7d33dc2779c1677d979fbf5baf5cba9187c972
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2016-01-08 13:07:15 -0500

    Use jenkins_service_spec.rb filename from master

--- test/integration/default/serverspec/jenkins_service_spec.rb
+++ test/integration/default/serverspec/jenkins_service_spec.rb
@@ -2,11 +2,21 @@ require 'serverspec'
 
 set :backend, :exec
 
-describe service('jenkins') do
-  it { should be_enabled }
+describe package('jenkins'), :if => os[:family] == 'redhat' do
+  it { should be_installed }
 end
 
-describe service('jenkins') do
+describe package('jenkins'), :if => os[:family] == 'ubuntu' do
+  it { should be_installed }
+end
+
+describe service('jenkins'), :if => os[:family] == 'redhat' do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe service('jenkins'), :if => os[:family] == 'ubuntu' do
+  it { should be_enabled }
   it { should be_running }
 end
 
@@ -14,7 +24,14 @@ describe port(8080) do
   it { should be_listening.with('tcp') }
 end
 
-describe command('curl -s http://127.0.0.1:8080/') do
+# RedHat defaults to having curl installed
+describe command('curl -s http://127.0.0.1:8080/'), :if => os[:family] == 'redhat' do
+	its(:exit_status) { should eq 0 }
+  its(:stdout) { should match /Log in<\/a> to create new jobs/ }
+end
+
+# Ubuntu defaults to having wget installed
+describe command('wget -q -O - http://127.0.0.1:8080/'), :if => os[:family] == 'ubuntu' do
 	its(:exit_status) { should eq 0 }
   its(:stdout) { should match /Log in<\/a> to create new jobs/ }
 end
