ce07216b1275f137def6e9949cfc041a53193f68
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2016-01-08 17:42:25 -0500

    Plugins to instantiate

---
+++ cookbooks/jenkins-config/attributes/jenkins-plugins.rb
@@ -0,0 +1,13 @@
+node.default['jenkins-config']['jenkins-plugins'] = [
+  {'name'    =>  'git',
+   'version' => '2.4.1'},
+  {'name'    => 'job-dsl'},
+  {'name'    => 'envinject'},
+  {'name'    => 'rvm'},
+  {'name'    => 'token-macro'},
+  {'name'    => 'ruby-runtime'},
+  {'name'    => 'ansicolor'},
+  {'name'    => 'delivery-pipeline-plugin'},
+  {'name'    => 'aws-codepipeline'}
+]
+
