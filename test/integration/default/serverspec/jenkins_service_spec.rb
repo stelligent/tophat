3aafd1e475dab9927af4fabf0b5f2ad25d020ddc
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2016-01-08 13:51:22 -0500

    Use cross platform http_get type

--- test/integration/default/serverspec/jenkins_service_spec.rb
+++ test/integration/default/serverspec/jenkins_service_spec.rb
@@ -24,14 +24,6 @@ describe port(8080) do
   it { should be_listening.with('tcp') }
 end
 
-# RedHat defaults to having curl installed
-describe command('curl -s http://127.0.0.1:8080/'), :if => os[:family] == 'redhat' do
-	its(:exit_status) { should eq 0 }
-  its(:stdout) { should match /Log in<\/a> to create new jobs/ }
-end
-
-# Ubuntu defaults to having wget installed
-describe command('wget -q -O - http://127.0.0.1:8080/'), :if => os[:family] == 'ubuntu' do
-	its(:exit_status) { should eq 0 }
-  its(:stdout) { should match /Log in<\/a> to create new jobs/ }
+describe http_get(8080, '127.0.0.1', '/') do
+  its(:body) { should match /Log in<\/a> to create new jobs/ }
 end
