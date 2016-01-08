2bdfb219caeacd8c35f8f4fa4d05923dec2cdf69
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2016-01-08 17:41:09 -0500

    Remove garbage, anti-testable package updates

--- cookbooks/jenkins-config/recipes/default.rb
+++ cookbooks/jenkins-config/recipes/default.rb
@@ -1,16 +1,7 @@
-if ['rhel'].include?(node['platform_family'])
-  # XXX should this instead do security updates instead of full
-  # system updates?
-  execute 'yum upgrade -y'
-end
-if ['debian'].include?(node['platform_family'])
-  include_recipe 'apt::default'
-  # XXX should this instead do security updates instead of full
-  # system updates?
-  execute 'apt-get upgrade -y' do
-    environment ({'DEBIAN_FRONTEND' => 'noninteractive'})
-  end
-end
+# Cookbook name:: jenkins-config
+# Recipe:: default
+#
+# Configure a Jenkins master
 
 include_recipe 'jenkins-config::prereqs'
 include_recipe 'java'
