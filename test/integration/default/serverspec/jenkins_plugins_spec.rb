66535e5a384a5e9f7cb674c577967262c03abb6b
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2016-01-08 17:43:21 -0500

    Ubuntu doesn't have curl by default

--- test/integration/default/serverspec/jenkins_plugins_spec.rb
+++ test/integration/default/serverspec/jenkins_plugins_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
 # RedHat has curl
-describe command('curl -s -o ~/jenkins-cli.jar http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar') do
-	its(:exit_status) { should eq 0 }
+describe command('curl -s -o ~/jenkins-cli.jar http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar'),
+         :if => os[:family] == 'redhat' do
+  its(:exit_status) { should eq 0 }
 end
 
 # Ubuntu has wget
-describe command('wget -q -O ~/jenkins-cli.jar http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar') do
-	its(:exit_status) { should eq 0 }
+describe command('wget -q -O ~/jenkins-cli.jar http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar'),
+         :if => os[:family] == 'ubuntu' do
+  its(:exit_status) { should eq 0 }
 end
 
 describe command('java -jar ~/jenkins-cli.jar -s http://127.0.0.1:8080/ list-plugins') do
