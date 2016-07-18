bde5aeda1a19c5d245855f6bbfdb59ef6e0f551b
Eric Kascic <eric.kascic@stelligent.com>
2016-07-18 17:00:15 -0400

    remove unnecessary divergence in testing jenkins package and service

--- test/integration/default/serverspec/jenkins_service_spec.rb
+++ test/integration/default/serverspec/jenkins_service_spec.rb
@@ -1,30 +1,20 @@
 require 'spec_helper'
 
-describe package('jenkins'), :if => os[:family] == 'redhat' do
+describe package('jenkins') do
   it { should be_installed }
 end
 
-describe package('jenkins'), :if => os[:family] == 'ubuntu' do
-  it { should be_installed }
-end
-
-describe service('jenkins'), :if => os[:family] == 'redhat' do
+describe service('jenkins') do
   it { should be_enabled }
   it { should be_running }
 end
 
-describe service('jenkins'), :if => os[:family] == 'ubuntu' do
-  it { should be_enabled }
-  it { should be_running }
-end
 
-describe port(8080), :if =>
-    %w(ubuntu redhat).include?(os[:family]) do
+describe port(8080), :if => %w(ubuntu redhat).include?(os[:family]) do
   it { should be_listening.with('tcp6') }
 end
 
-describe port(8080), :if =>
-    %w(amazon).include?(os[:family]) do
+describe port(8080), :if => %w(amazon).include?(os[:family]) do
   it { should be_listening.with('tcp') }
 end
 
