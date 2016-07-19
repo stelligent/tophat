c1fd2a25f0e2fa0bf20f98f5b3db0e79798dada1
Eric Kascic <eric.kascic@stelligent.com>
2016-07-19 14:50:09 -0400

    force myself to stop wasting time with ubuntu java mknod problem and move onto more important stuff

--- cookbooks/jenkins-config/recipes/prereqs.rb
+++ cookbooks/jenkins-config/recipes/prereqs.rb
@@ -4,8 +4,8 @@ node['jenkins-config']['prereq']['packages'].each do |package|
   package package
 end
 
-apt_package 'openjdk-7-jdk' do
-  options '--no-install-recommends'
-  version node['java']['openjdk_version'] if node['java']['openjdk_version']
-  only_if { node['platform_family'] == 'debian' }
-end
+# apt_package 'openjdk-7-jdk' do
+#   options '--no-install-recommends'
+#   version node['java']['openjdk_version'] if node['java']['openjdk_version']
+#   only_if { node['platform_family'] == 'debian' }
+# end
