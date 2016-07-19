cb9558ead535ce3570fa72b6c26482cc765670f0
Eric Kascic <eric.kascic@stelligent.com>
2016-07-19 10:32:34 -0400

    hmph.  doesn't seem to like a user var as a builder name so... duplicate the builder and see if that will at least work the way we desire even though it sort of sucks

--- packer-build.sh
+++ packer-build.sh
@@ -32,7 +32,7 @@ ssh_username=$(ruby -e "require 'yaml'" \
 
 berks vendor "$berks_cookbook_path"
 packer build \
-    -var "target_os_family=${target_os_family}"  \
+    -only ${target_os_family}  \
     -var "vpc_id=$vpc_id" \
     -var "subnet_id=$subnet_id" \
     -var "berks_cookbooks_path=$berks_cookbook_path" \
