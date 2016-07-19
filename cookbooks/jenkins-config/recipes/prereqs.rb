83f456e3b4affb7797b7fd82568c5358861d251d
Eric Kascic <eric.kascic@stelligent.com>
2016-07-19 14:31:24 -0400

    try to beat it to the punch for docker

--- cookbooks/jenkins-config/recipes/prereqs.rb
+++ cookbooks/jenkins-config/recipes/prereqs.rb
@@ -3,3 +3,9 @@ include_recipe 'yum-epel::default' if node['platform_family'] == 'rhel'
 node['jenkins-config']['prereq']['packages'].each do |package|
   package package
 end
+
+apt_package 'openjdk-7-jdk' do
+  options '--no-install-recommends'
+  version node['java']['openjdk_version'] if node['java']['openjdk_version']
+  only_if { node['platform_family'] == 'debian' }
+end
