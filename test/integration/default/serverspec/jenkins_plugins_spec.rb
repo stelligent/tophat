ec8ba3039d86dcda1629daa5fb6193ce1c178b14
Eric Kascic <eric.kascic@stelligent.com>
2016-07-19 12:11:47 -0400

    add the pipeline plugin to the install

--- test/integration/default/serverspec/jenkins_plugins_spec.rb
+++ test/integration/default/serverspec/jenkins_plugins_spec.rb
@@ -18,9 +18,32 @@ end
 # version checking
 describe command('java -jar ~/jenkins-cli.jar -s http://127.0.0.1:8080/ list-plugins') do
   its(:exit_status) { should eq 0 }
-  plugins = %w(git job-dsl envinject rvm token-macro
-              ruby-runtime ansicolor matrix-auth
-              aws-codepipeline)
+  plugins = %w(
+    git
+    job-dsl
+    envinject
+    rvm
+    token-macro
+    ruby-runtime
+    ansicolor
+    matrix-auth
+    aws-codepipeline
+    workflow-aggregator
+    workflow-support
+    workflow-cps
+    pipeline-stage-step
+    pipeline-stage-view
+    workflow-cps-global-lib
+    pipeline-input-step
+    workflow-basic-steps
+    workflow-multibranch
+    workflow-scm-step
+    workflow-job
+    workflow-durable-task-step
+    workflow-step-api
+    pipeline-build-step
+    workflow-api
+  )
   plugins.each do |plugin|
     its(:stdout) { should match /#{plugin}/ }
   end
